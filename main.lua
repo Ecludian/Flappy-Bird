38c667268f5d7ee2407d48ea0ba72e5d45476888
Ecludian <charelnugroho@gmail.com>
2022-06-21 14:51:25 +0700

    The Parallax Update

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 push = require('push')
 
+Class = require('class')
+
+require 'Bird'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -7,15 +11,24 @@ V_WIDTH = 512
 V_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
 
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Flappy Bird')
 
-    push:setupScreen(V_WIDTH, V_HEIGHT, WINDOW_WIDTH, WINDOW_WIDTH, {
+    push:setupScreen(V_WIDTH, V_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true;
         fullscreen = false;
         resizable = true
@@ -32,9 +45,18 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+    % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+    % V_WIDTH
+end
+
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, V_HEIGHT - 16)
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(ground, -groundScroll, V_HEIGHT - 16)
+    bird:render()
     push:finish()
 end
